696fe38faa7a30bd83311959b31e8a2334ab50ae
Noorjahan Mukammel <jahan@Noorjahans-MacBook-Pro.local>
2018-11-07 15:51:40 +0600

    test new

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Alpine-Coffee-Update
+express-sendfile
+================
+
+Code for the tutorial by @sevilayha: Use ExpressJS to Deliver HTML Files
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+// app.use(bodyParser.json()); // support json encoded bodies
+// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+
+// viewed at http://localhost:8080
+app.use(express.static(__dirname+ '/views')); 
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+
+
+
+app.post('/user', function(req, res){
+   // res.sendFile(path.join(__dirname + '/views/index.html'));
+    console.log("test")
+    response = {
+        first_name : req.body.first_name,
+        last_name : req.body.last_name,
+        gender: req.body.gender
+        };
+    
+    //this line is optional and will print the response on the command prompt
+    //It's useful so that we know what infomration is being transferred 
+    //using the server
+    console.log(response);
+    
+    //convert the response in JSON format
+    //res.end(JSON.stringify(response));
+});
+
+
+app.listen(8080);
+console.log('Server started! At http://localhost:' + '8080');
